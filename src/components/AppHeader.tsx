7bc7dd7e4db7c89e90bd36b4810138c0020ada4f
caicodes <caicodes1@gmail.com>
2023-10-03 06:27:12 -0600

    dialog props fix, header bg adjust

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./ModeToggle"
 
 export const AppHeader = () => {
   return (
-    <div className="flex justify-between p-4 bg-slate-300 dark:bg-slate-900 place-items-center">
+    <div className="flex justify-between p-4 bg-slate-300/50 dark:bg-slate-900/20 place-items-center">
       <AppLogo />
       <ModeToggle />
     </div>
